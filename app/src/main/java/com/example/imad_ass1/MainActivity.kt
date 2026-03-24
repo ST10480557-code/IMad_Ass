3f4207c360d65f57a4b35965f46a8fccbe87f52e
ST10480557-code <st10480557@myemeris.edu.za>
2026-03-24 15:53:15 +0200

    added toast message

--- app/src/main/java/com/example/imad_ass1/MainActivity.kt
+++ app/src/main/java/com/example/imad_ass1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -64,8 +65,14 @@ var Time : String
                 tvTitle.text = "Leave a thoughtful comment on a friend's post"
 
             else{
-                tvTitle.text = "Please enter valid information"
+                Toast.makeText(
+                    this, " Please enter valid information",
+                    Toast.LENGTH_SHORT
+
+                ).show()
             }
+
+
 
 
         }
